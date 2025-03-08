e4a8cdba366353df4160673d7365d81b0010224d
thinhnguyen-10 <thinhducnguye2007@gmail.com>
2025-03-08 12:32:48 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = pass.value
+  if (u=="DucThinh" && p=="123456789")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
